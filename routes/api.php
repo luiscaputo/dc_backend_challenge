4d16b9a422c6dde4e284fc3ef02eaa833bb98434
luiscaputo <luiscaputo15@gmail.com>
2022-11-04 04:50:09 +0100

    feat: Added Products Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProviderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -34,3 +35,11 @@ Route::get('/providers/{id}', [ProviderController::class, 'show']);
 Route::post('/providers', [ProviderController::class, 'store']);
 Route::put('/providers/{id}', [ProviderController::class, 'update']);
 Route::delete('/providers/{id}', [ProviderController::class, 'destroy']);
+
+
+// Products routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
